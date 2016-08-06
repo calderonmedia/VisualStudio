aed20e24fa53c26622a8e2151c74057821652856
Jesus Calderon <calderonmedia@gmail>
2016-08-06 12:55:18 -0700

    There was a modification in the DB

--- itt.sql
+++ itt.sql
@@ -1,1 +1,9 @@
 Select * from DB where FNAME = "John"
+Select * from DB where LASTNAME
+adsvahbeneqan fbqanaernten
+ afvbwrgawgbreahbre
+ asbarebarehaerhreaha
+ abbarehbarebahrehg
+ avqgrbrwagrwagrh
+ gvarqgbqrweghqrweh
+ `
